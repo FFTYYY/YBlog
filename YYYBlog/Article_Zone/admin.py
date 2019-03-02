f2727eca7e75d6d1a7244f890ca7801508f8fecb
FFTYYY <1004473299@qq.com>
2019-03-02 19:31:40 +0800

    前端的文章排序 & 后台按深度排序
    另外，修复了一个bug：后台选父亲的时候不能使用「子节点列表」功能
    另外，删除了进度.txt，现在用github的projects功能来管理进度，而非一个txt文件

--- YYYBlog/Article_Zone/admin.py
+++ YYYBlog/Article_Zone/admin.py
@@ -1,24 +1,70 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from .models import *
 from django.utils.html import format_html
+from .utils.relationship import *
 
 def 字符串截短(s , cut_len = 20):
 	if len(s) > cut_len:
 		s = s[:cut_len] + "..."
 	return s
 
+
+class 按深度查找(admin.SimpleListFilter):
+	cut_len = 5
+	title = '深度'
+	parameter_name = 'depth'
+
+	def 深度字符串(self , val):
+		if val > self.cut_len:
+			return "深于%d" % self.cut_len
+		return str(val)
+
+	def 节点深度(self , 点):
+		return 节点深度(点 , self.cut_len)
+
+	def lookups(self, request, model_admin):
+		return [ (x+1 , self.深度字符串(x+1)) for x in range(self.cut_len + 1)]
+
+	def queryset(self, request, queryset):
+		if self.value() is None:
+			return queryset
+		符合条件的节点 = [点.id for 点 in 节点.objects.all() if str(self.节点深度(点)) == self.value()]
+		return queryset.filter(id__in = 符合条件的节点)
+
 class 节点Admin(admin.ModelAdmin):
-	list_filter = ["节点类型"]
-	list_display = ["名" , "子节点" , "父节点" , "节点类型"]
-	raw_id_fields = ["父",]
-	search_fields = ["名"]
+	list_filter = ["节点类型" , 按深度查找 , ]
+	list_display = ["名" , "子节点" , "父节点" , "节点类型" , "深度" , ]
+	raw_id_fields = ["父" , ]
+	search_fields = ["名" , ]
 
 	def 子节点(self , 对象):
-		return format_html("<a href = ./?父__exact=%s>子节点列表</a>" % (对象.id))
+		增加地址 = "?父__exact=%s"% (对象.id)
+		地址 = self.request.get_full_path()
+		if 地址.find("_popup=1") >= 0:
+			增加地址 += "&_popup=1"
+		地址 = self.request.path + 增加地址
+		return format_html("<a href = %s>子节点列表</a>" % (地址))
+
 	def 父节点(self , 对象):
-		if 对象.父:
-			return format_html("<a href = ./?id__exact=%s>%s</a>" % (对象.父.id , 对象.父))
-		return "无"
+		if not 对象.父:
+			return "无"
+		增加地址 = "?id__exact=%s"% (对象.父.id)
+		地址 = self.request.get_full_path()
+		if 地址.find("_popup=1") >= 0:
+			增加地址 += "&_popup=1"
+		地址 = self.request.path + 增加地址
+		return format_html("<a href = %s>%s</a>" % (增加地址 , 对象.父))
+
+	def 深度(self , 对象):
+		return 节点深度(对象)
+
+	def change_view(self, request, object_id, form_url='', extra_context=None):
+		self.request = request
+		return super(节点Admin, self).change_view(request, object_id, form_url=form_url, extra_context=extra_context)
+
+	def changelist_view(self, request, extra_context=None):
+		self.request = request
+		return super(节点Admin,self).changelist_view(request, extra_context=extra_context)
 
 
 class 留言Admin(admin.ModelAdmin):
--- YYYBlog/Article_Zone/utils/relationship.py
+++ YYYBlog/Article_Zone/utils/relationship.py
@@ -14,4 +14,12 @@ def 获取兄弟节点列表(节点):
 	父 = 节点.父
 	if not 父:
 		return []
-	return 父.子.all()
+	return 父.子.order_by("排序依据")
+
+
+def 节点深度(点 , cut_len = -1):
+	列表 = 获取祖先节点列表(点)
+	长度 = len(列表) + 1
+	if cut_len > 0 and 长度 > cut_len:
+		长度 = cut_len + 1
+	return 长度
