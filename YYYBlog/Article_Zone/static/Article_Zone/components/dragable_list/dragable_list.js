6a5b79e21c9245c4928bd2b1bbbb1fc6b3b690c1
FFTYYY <1004473299@qq.com>
2019-05-29 21:24:11 +0800

    修改dragble_list，加入动画
    目前有一摩尔bug

--- YYYBlog/Article_Zone/static/Article_Zone/components/dragable_list/dragable_list.js
+++ YYYBlog/Article_Zone/static/Article_Zone/components/dragable_list/dragable_list.js
@@ -6,8 +6,66 @@ showing_index = 0;
 now_showing_in = false
 mc_in = false
 hmc_in = false
+the_div = 0;
 		
 hidden_main_cont = document.getElementById("hidden_main_cont")
+
+function showing(who)
+{
+	return !(who.classList.contains("hidden") || who.classList.contains("unshowing"))
+}
+
+function make_hidden(who , tag = "hidden")
+{//给退出添加动画
+	old_s = showing(who)
+		who.classList.remove("ining")
+
+
+	if(old_s)
+	{
+		let the_guy = who
+		let now_tag = tag
+		the_guy.classList.add("leaving")
+		the_guy.should_hid = true;
+		setTimeout(
+			function()
+			{
+				if(the_guy.should_hid)
+					the_guy.classList.add(now_tag)
+				if(now_tag == "hidden")
+					the_guy.classList.add(now_tag)
+				the_guy.classList.remove("leaving")
+			} , 
+			500
+		)
+	}
+}
+
+function unhidden(who , tag = "hidden")
+{
+	old_s = showing(who)
+	who.classList.remove(tag)
+	new_s = showing(who)
+	
+	if(tag == "hidden")
+		who.should_hid = false;
+	who.classList.remove("leaving")
+
+	if((!old_s) && (new_s))
+	{
+		let the_guy = who
+		let now_tag = tag
+		the_guy.classList.add("ining")
+		setTimeout(
+			function()
+			{
+				the_guy.classList.remove("ining")
+			} , 
+			500
+		)
+	}
+
+}
 
 function switch_hidden_main_cont_state(on = true)
 {
@@ -26,11 +84,11 @@ function switch_hidden_main_cont_state(on = true)
 
 function unshow_when_mouseleave()
 {
-	setInterval(
+	setTimeout(
 		function(){
 			if(!(now_showing_in || mc_in || hmc_in))
 			{
-				now_showing.classList.add("hidden")
+				make_hidden(now_showing , "hidden")
 
 				switch_hidden_main_cont_state(false)
 			}
@@ -56,13 +114,17 @@ function update_now_showing(ind)
 
 	for(var i = 0;i < showing_list.length;i++)
 	{
-		showing_list[i].classList.add("unshowing");
+		if(showing(showing_list[i]))
+		{
+			this_man = showing_list[i];
+			make_hidden(this_man , "unshowing");
+		}
 	}
-	now_showing.classList.remove("unshowing");
+	unhidden(now_showing , "unshowing");
 }
 
 function locate_now_showing()
-{
+{//根据当前按钮位置变换列表的位置
 	if(the_div.offsetLeft < window.innerWidth / 2)
 		now_showing.style.left = main_cont.offsetWidth
 	else now_showing.style.left = -now_showing.offsetWidth
@@ -91,16 +153,22 @@ function dragble_list_init(main_cont , showing_lis)
 		ele.style.width = hidden_main_cont.offsetWidth
 		ele.style.height = hidden_main_cont.offsetHeight
 
+		ele["should_hid"] = false;
 	}
 
 	showing_list = showing_lis;
 
 	main_cont.onmousemove = function(){
 		locate_now_showing()
+
 	}
 	main_cont.onmouseenter = function (){
+		console.log("sasaas")
 		mc_in = true
-		now_showing.classList.remove("hidden")
+
+		unhidden(now_showing , "hidden");
+		unhidden(now_showing , "unshowing");
+		console.log(now_showing.classList)
 		switch_hidden_main_cont_state(true)
 		locate_now_showing()
 	}
@@ -120,7 +188,7 @@ function dragble_list_init(main_cont , showing_lis)
 
 		showing_index = (showing_index + showing_list.length) % showing_list.length
 		update_now_showing(showing_index);
-		now_showing.classList.remove("hidden");
+		unhidden(now_showing , "hidden");
 		locate_now_showing()
 	}
 
@@ -136,3 +204,29 @@ function dragble_list_init(main_cont , showing_lis)
 	update_now_showing(0)
 	switch_hidden_main_cont_state(false)
 }
+
+function mouse_out(mx , my , who)
+{
+	leff = who.offsetLeft + the_div.offsetLeft
+	topp = who.offsetTop + the_div.offsetTop
+	//console.log(leff , topp , mx , my)
+
+	return mx < leff || mx > leff + who.offsetWidth ||
+			my < topp || my > topp + who.offsetHeight;
+}
+
+document.addEventListener('mousemove', function(e) {
+	mx = e.clientX
+	my = e.clientY
+
+	if(e.movementX || e.movementY)
+	{
+		mc_in = !mouse_out(mx,my,main_cont);
+		hmc_in = !mouse_out(mx,my,hidden_main_cont);
+		if(showing(now_showing))
+			now_showing_in = !mouse_out(mx,my,now_showing)
+
+		unshow_when_mouseleave();
+	}
+
+})
