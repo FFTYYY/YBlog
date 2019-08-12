e8581ea46d34c19e161143b63db5df86a7b4b574
FFTYYY <1004473299@qq.com>
2019-08-13 00:12:31 +0800

    修复连续换行只显示一个的错误，修复后台字体选择的bug

--- YYYBlog/Universe/static/ckeditor/ckeditor/config.js
+++ YYYBlog/Universe/static/ckeditor/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	config.extraPlugins = 'YYYSpliter,YYYRegulizeSpliter,' + config.extraPlugins; 
-	config.font_names = '宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;' + config.font_names ;
+	config.font_names = '宋体/宋体;黑体/黑体;仿宋/FangSong;楷体/KaiTi;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;' + config.font_names ;
 	//config.extraPlugins = "yyy_spliter," + config.extraPlugins;
 
 };
