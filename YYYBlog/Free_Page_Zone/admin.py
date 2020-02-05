2f49614dff42df6b4d5de0aa90080275333c893f
YYY <1004473299@qq.com>
2020-02-06 02:28:22 +0800

    增加页文件功能，自由页面可以将文件内容作为参数

--- YYYBlog/Free_Page_Zone/admin.py
+++ YYYBlog/Free_Page_Zone/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(页)
+class 页文件Inline(admin.TabularInline):
+	model = 页文件
+	extra = 0
+
+
+class 页Admin(admin.ModelAdmin):
+	inlines = [ 页文件Inline, ]
+
+admin.site.register(页 , 页Admin)
+admin.site.register(页文件)
