72414fc27fb35a02c51baef29431964b81e78742
YYY <1004473299@qq.com>
2020-02-06 01:04:30 +0800

    增强自由页面的功能

--- YYYBlog/Free_Page_Zone/views.py
+++ YYYBlog/Free_Page_Zone/views.py
@@ -9,4 +9,15 @@ def 获取页(request , 页地址 , 其他参数 = ""):
 	if not 页许可查询(request , 此页):
 		raise Http404
 
-	return HttpResponse(此页.内容)
+	if 此页.类型 == 0:
+		return HttpResponse(此页.内容)
+	elif 此页.类型 == 1:
+		variables = {
+			"param" : 其他参数 , 
+		}
+		run_func = {}
+		exec(此页.内容)
+
+		return HttpResponse(run_func["run"](variables))
+
+	raise Http404 
