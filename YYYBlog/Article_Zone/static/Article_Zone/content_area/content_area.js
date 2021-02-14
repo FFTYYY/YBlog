5081f0ffa1a539782f4457fe04389cc0caad8a67
YYY <1004473299@qq.com>
2021-02-14 19:33:49 +0800

    small changes in frontend

--- YYYBlog/Article_Zone/static/Article_Zone/content_area/content_area.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(
-	function() { 
-		$(".content-area .content").niceScroll({cursorborder:"",cursorcolor:"#657899",boxzoom:false});
-		setInterval(function(){$(".content-area .content").getNiceScroll().resize()} , 500)
-	}
-);
