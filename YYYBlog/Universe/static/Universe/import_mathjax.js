8442cc4842c6979c1edc491e8d45457995211e73
YYY <1004473299@qq.com>
2021-05-04 20:39:09 +0800

    尝试修复mathjax渲染不出来的问题

--- YYYBlog/Universe/static/Universe/import_mathjax.js
+++ YYYBlog/Universe/static/Universe/import_mathjax.js
@@ -1,20 +1,5 @@
-
-function import_mathjax(){
-	var mj_sc = document.createElement("script");
-	mj_sc.setAttribute("type","text/javascript");
-	mj_sc.setAttribute("src","/static/Universe/MathJax/MathJax.js");
-	document.head.appendChild(mj_sc);
-
-	var mj_cf = document.createElement("script");
-	mj_cf.setAttribute("type","text/x-mathjax-config");
-	mj_cf.text = `
-	MathJax.Hub.Config({
-		extensions: ["tex2jax.js"],
-		jax: ["input/TeX","output/HTML-CSS"],
-		tex2jax: {inlineMath: [["$","$"]]}
-	});
-	`
-	document.head.appendChild(mj_cf);
-}
-
-import_mathjax()
+MathJax.Hub.Config({
+	extensions: ["tex2jax.js"],
+	jax: ["input/TeX","output/HTML-CSS"],
+	tex2jax: {inlineMath: [["$","$"]]}
+})
