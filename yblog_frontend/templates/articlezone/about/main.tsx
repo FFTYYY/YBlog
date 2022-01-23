6f0eb01dc8cf80069413849c88f23af2d93f8cde
Yongyi <1004473299@qq.com>
2022-01-24 00:41:06 +0800

    哈哈哈哈！

--- yblog_frontend/templates/articlezone/about/main.tsx
+++ yblog_frontend/templates/articlezone/about/main.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<div> about! </div>
 	</React.StrictMode>,
 	document.getElementById("root")
 )
