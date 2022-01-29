d0153503a3b925434aaeb6097b298af5f514fa7a
Yongyi <1004473299@qq.com>
2022-01-30 01:44:53 +0800

    完了全完了

--- yblog/articlezone/views/views.py
+++ yblog/articlezone/views/views.py
@@ -4,11 +4,6 @@ from .utils import allow_acess
 
 @allow_acess
 def node_view(request , node_id):
-    print (request.method)
-    print (request.POST)
-    print (request.body)
-    print (request.GET)
-    print ("---------")
     return render(request , "articlezone/article_editor_index.html" , {
         "node_id": node_id , 
     })
