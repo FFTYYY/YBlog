aba5793633d955e7b027875185b20900d15a5333
Yongyi <1004473299@qq.com>
2022-02-04 00:17:04 +0800

    增加组件

--- yblog_frontend/templates/articlezone/components/index.tsx
+++ yblog_frontend/templates/articlezone/components/index.tsx
@@ -1,6 +1,6 @@
-import { brightwords_erenderer } from "./interface_renderer"
-import { brightwords_orenderer } from "./output_renderer"
-import { brightwords_style } from  "./styles"
+import { brightwords_orenderer , followwords_orenderer , mount_orenderer , display_orenderer } from "./output_renderer"
+import { brightwords_erenderer , followwords_erenderer , mount_erenderer , display_erenderer } from "./interface_renderer"
+import { brightwords_style , followwords_style , dimwords_style , mount_style , display_style } from  "./styles"
 import { YEditor } from "@ftyyy/ytext"
 import { OutRenderer , EditorCore } from  "@ftyyy/ytext"
 
@@ -8,16 +8,26 @@ export {withAllStyles_Editor , withAllStyles_Output , withAllStyles_Interface}
 
 function withAllStyles_Editor(core:EditorCore): EditorCore{
     core.add_groupstyle(brightwords_style)
+    core.add_groupstyle(followwords_style)
+    core.add_groupstyle(mount_style)
+    core.add_groupstyle(display_style)
+    core.add_abstractstyle(dimwords_style)
     return core
 }
 
 function withAllStyles_Interface(yeditor: YEditor): YEditor{
-    yeditor.update_renderer(brightwords_erenderer , "group" , "昭言")
+    yeditor.update_renderer(brightwords_erenderer   , "group" , "昭言")
+    yeditor.update_renderer(followwords_erenderer   , "group" , "随言")
+    yeditor.update_renderer(mount_erenderer         , "group" , "裱示")
+    yeditor.update_renderer(display_erenderer       , "group" , "彰示")
     return yeditor
 }
 
 
 function withAllStyles_Output(output_renderer: OutRenderer): OutRenderer{
-    output_renderer.update_renderer( brightwords_orenderer, "group" , "昭言" )
+    output_renderer.update_renderer( brightwords_orenderer  , "group" , "昭言" )
+    output_renderer.update_renderer( followwords_orenderer  , "group" , "随言" )
+    output_renderer.update_renderer( mount_orenderer        , "group" , "裱示" )
+    output_renderer.update_renderer( display_orenderer      , "group" , "彰示" )
     return output_renderer
 }
--- yblog_frontend/templates/articlezone/components/styles.tsx
+++ yblog_frontend/templates/articlezone/components/styles.tsx
@@ -1,7 +1,7 @@
-import { GroupStyle , new_default_group } from "@ftyyy/ytext"
+import { GroupStyle , new_default_group , AbstractStyle} from "@ftyyy/ytext"
 
-export { brightwords_style }
-export {brightwords_erenderer}
+export { brightwords_style , followwords_style , dimwords_style , mount_style , display_style }
+export { brightwords_erenderer , followwords_erenderer , mount_erenderer , display_erenderer }
 
 var [brightwords_style , brightwords_erenderer] = new_default_group(
     "昭言" , 
@@ -11,4 +11,25 @@ var [brightwords_style , brightwords_erenderer] = new_default_group(
     } , 
 )
 
+var [followwords_style , followwords_erenderer] = new_default_group(
+    "随言" , 
+    {
+        title: "随言" , 
+    } , 
+)
+
+var dimwords_style = new AbstractStyle("穆言" , {})
+
+var [ mount_style , mount_erenderer] = new_default_group(
+    "裱示" , 
+    {
+        title: "裱示" , 
+    } , 
+)
+var [ display_style , display_erenderer] = new_default_group(
+    "彰示" , 
+    {
+        title: "彰示" , 
+    } , 
+)
 
