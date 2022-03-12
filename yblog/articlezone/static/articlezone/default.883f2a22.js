8987e411016029ebc855a782c5c307b53aab52d8
Yongyi <1004473299@qq.com>
2022-03-13 05:05:28 +0800

    rebuild

---
+++ yblog/articlezone/static/articlezone/default.883f2a22.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.9d8db203.js";import{R as d,j as e,a as o}from"./vendor.d26df5e7.js";d.render(e(o.StrictMode,{children:e("div",{children:" about! "})}),document.getElementById("root"));
