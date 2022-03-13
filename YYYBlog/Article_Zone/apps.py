6af17b8ddf4c81c4ec05229e7f15d54753f2b4a4
Yongyi <1004473299@qq.com>
2022-03-14 01:14:37 +0800

    删除对老版本的库的git记录

--- YYYBlog/Article_Zone/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArticleZoneConfig(AppConfig):
-    name = 'Article_Zone'
--- YYYBlog/Universe/templatetags/universe_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.defaultfilters import stringfilter
-
-register = template.Library()
-
-@register.filter
-@stringfilter
-def lks(arg1, arg2):
-	return str(arg1) + str(arg2)
