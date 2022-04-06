5c0ecc815de249563e556fbde5aaf71b7e9f94d2
Yongyi <1004473299@qq.com>
2022-04-07 05:08:13 +0800

    是bug在fix我！是bug在fix我！

--- yblog_frontend/templates/articlezone/base/construction/titleword.tsx
+++ yblog_frontend/templates/articlezone/base/construction/titleword.tsx
@@ -4,6 +4,8 @@ import { get_param_val } from "../../../../lib"
 
 export { TitleWord }
 
+var id2title = {} // 缓存。
+
 /** 这个组件异步加载一个节点的标题，等加载好了就现实出来。 */
 class TitleWord extends React.PureComponent<{
     node_id: number
@@ -19,8 +21,12 @@ class TitleWord extends React.PureComponent<{
     }
 
     async componentDidMount() {
-        let root = await Interaction.get.content(this.props.node_id)
-        this.setState({title: get_param_val(root , "title") as string}) 
+        if(id2title[this.props.node_id] == undefined){
+            let root = await Interaction.get.content(this.props.node_id)
+            let title = get_param_val(root , "title")
+            id2title[this.props.node_id] = title
+        }
+        this.setState({title: id2title[this.props.node_id] as string}) 
     }
 
     render(){
