a76d80a3e7f9ec18076ad759f745172408524087
Yongyi <1004473299@qq.com>
2022-04-07 01:17:19 +0800

    让修改节点的结构变得更便利了...大概...

--- yblog_frontend/templates/articlezone/base/construction/titleword.tsx
+++ yblog_frontend/templates/articlezone/base/construction/titleword.tsx
@@ -5,7 +5,11 @@ import { get_param_val } from "../../../../lib"
 export { TitleWord }
 
 /** 这个组件异步加载一个节点的标题，等加载好了就现实出来。 */
-class TitleWord extends React.Component<{node_id: number} , {title: string | undefined}>{
+class TitleWord extends React.PureComponent<{
+    node_id: number
+} , {
+    title: string | undefined
+}>{
     constructor(props){
         super(props)
 
