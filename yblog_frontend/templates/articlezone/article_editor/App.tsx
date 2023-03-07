37f002c93dd5f71c2d8a23fd10747b2adf3d3f9e
Yongyi <yongyi@umich.edu>
2023-03-07 03:50:38 -0500

    woshishabi

--- yblog_frontend/templates/articlezone/article_editor/App.tsx
+++ yblog_frontend/templates/articlezone/article_editor/App.tsx
@@ -254,14 +254,13 @@ class App extends  React.Component<{}, {
 				left: "1%" , 
 				height: "96%" , 
 				width: "98%" , 
-				textAlign: "center" , 
 			}}>
 				<Card sx={{
 					position: "absolute" , 
 					left: "0" , 
 					width: "auto" ,
 					display: "flex" , 
-					flexDirection: "column"
+					flexDirection: "column" , 
 				}}>
 					<SaveButton save_func = {me.save_content.bind(me)}/>
 					<FileManageButton />
