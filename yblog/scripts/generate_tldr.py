a9bf86308dce8b28f6f1e5894e14a8fa669290e8
Yongyi <yongyi@umich.edu>
2023-03-23 00:44:57 -0400

    增加了一个自动为所有文章生成tldr的脚本

---
+++ yblog/scripts/generate_tldr.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import os , sys
+my_path = Path( os.path.dirname(os.path.abspath(__file__)) )
+
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yblog.settings")
+django.setup()
+
+import pdb
+import json
+from tqdm import tqdm
+from articlezone.models.models import Node
+
+if __name__ == "__main__":
+
+    nodes = Node.objects.all()
+
+    res = {}
+    pbar = tqdm( nodes )
+    for x in pbar:
+        x.update_tldr()
+        x.save()
+        pbar.set_description_str("now %d" % x.id)
+    print ("done!")
