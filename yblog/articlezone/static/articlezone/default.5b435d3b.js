a60f73d19e164851c6180a385016e02cd166a344
Yongyi <yongyi@umich.edu>
2023-07-13 05:50:09 -0400

    草

---
+++ yblog/articlezone/static/articlezone/default.5b435d3b.js
@@ -0,0 +1,1 @@
+import{R as e,j as t,a,b as r}from"./titleword.c58b97da.js";import{M as o}from"./math.7286f288.js";e.render(t(a.StrictMode,{children:r("div",{children:[t(o,{children:" $12$ "})," about! "]})}),document.getElementById("root"));
