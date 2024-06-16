7ae3477946854930d54a4decfa61e91994616d1c
Yongyi <1004473299@qq.com>
2024-06-16 00:25:48 -0400

    add nginx

---
+++ nginx/make_nginx.py
@@ -0,0 +1,31 @@
+from config import DOMAIN, LOCALPATH, NGINX_PATH, NGINX_ENABLED_PATH
+import os 
+
+template = """
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name {DOMAIN};
+
+    location / {
+        proxy_pass http://127.0.0.1:8001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location /static/ {
+        alias {LOCALPATH}/static/;
+    }
+
+    location /media/ {
+        alias {LOCALPATH}/media/;
+    }
+}
+""".format(DOMAIN = DOMAIN, LOCALPATH = LOCALPATH)
+
+if __name__ == "__main__": 
+    with open(NGINX_PATH, "w") as f:
+        f.write(template)
+    os.system(f"sudo ln -s {NGINX_PATH} {NGINX_ENABLED_PATH}")
